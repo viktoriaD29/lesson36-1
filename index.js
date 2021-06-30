7e90f4399a7fb66289ea12a6603f8e7a76e75749
Viktoria <doduchenko29@gmail.com>
2021-06-30 20:31:48 +0300

    add

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export const fetchUser = async (userId) => {
     const userData = await response.json();
     return Promise.resolve(userData);
   } catch {
-    throw new Error();
+    throw new Error('Error');
   }
 };
 //fetchUser('google')
