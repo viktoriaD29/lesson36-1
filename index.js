f22d410bfcf86e4368d00f61050ec6c26652aaa2
Viktoria <doduchenko29@gmail.com>
2021-06-30 05:04:17 +0300

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+export const fetchUser = async (userId) => {
+  try{
+    const response = await fetch(`https://api.github.com/users/${userId}`)
+    if(!response.ok) {
+      return null
+    }
+    const userData = await response.json()
+    return userData
+  } catch (err) {
+    alert(err)
+  }
+}
+/*fetchUser('google')
+  .then(userData => console.log(userData))
+  .catch(err => alert(err.message))*/
